9c6f58bcb3842f61c0f3496780e5afcccdc5be1d
Matthew Hu <matthu24@gmail.com>
2018-03-05 18:21:32 -0800

    Add user model

---
+++ db/migrate/20180306011900_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string "username", null: false
+      t.string "password_digest", null: false
+      t.string "session_token", null: false
+      t.timestamps
+      t.index ["session_token"], name: "index_users_on_session_token", unique: true
+      t.index ["username"], name: "index_users_on_username", unique: true
+    end
+  end
+end
