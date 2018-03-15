82c03f0eda472b72b930e2601e5161de04d3676f
Matthew Hu <matthu24@gmail.com>
2018-03-14 19:03:32 -0700

    Finish session action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,12 +2,32 @@ import { postUser, postSession, deleteSession } from '../utils/session';
 
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser= currentUser => ({
   type: RECEIVE_CURRENT_USER,
   currentUser
 })
 
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
 export const signup = user => dispatch => (
-  postUser(user).then(user => dispatch(receiveCurrentUser(user)))
+  postUser(user).then(user => dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  )
+)
+
+export const login = user => dispatch => (
+  postSession(user).then(user => dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  )
+)
+
+export const logout = () => dispatch => (
+  deleteSession().then(user => receiveCurrentUser(null))
 )
